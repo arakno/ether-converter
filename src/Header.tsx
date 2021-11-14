6a3c2547923061dc5147a6f12de67bfc41a7cfb9
arakno <paulobasto@gmail.com>
2021-11-14 22:35:46 +0000

    Add table data.

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,43 @@ import Button from './Button'
 
 const Panel = () => (
   <div id="panel" className="panel-converter">
-    Some Results
+    <table>
+      <tr>
+        <td><strong>wei</strong></td>
+        <td>1 wei</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td><strong>Kwei (babbage)</strong></td>
+        <td>1e3 wei</td>
+        <td>1,000</td>
+      </tr>
+      <tr>
+        <td><strong>Mwei (lovelace)</strong></td>
+        <td>1e6 wei</td>
+        <td>1,000,000</td>
+      </tr>
+      <tr>
+        <td><strong>Gwei (shannon)</strong></td>
+        <td>1e9 wei</td>
+        <td>1,000,000,000</td>
+      </tr>
+      <tr>
+        <td><strong>microether (szabo)</strong></td>
+        <td>1e12 wei</td>
+        <td>1,000,000,000,000</td>
+      </tr>
+      <tr>
+        <td><strong>milliether (finney)</strong></td>
+        <td>1e15 wei</td>
+        <td>1,000,000,000,000,000</td>
+      </tr>
+      <tr>
+        <td><strong>ether</strong></td>
+        <td>1e18 wei</td>
+        <td>1,000,000,000,000,000,000</td>
+      </tr>
+    </table>
   </div>
 )
 class Header extends Component<any, any> {
