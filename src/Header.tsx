82220aab009d60c842cea4f2a220494f83305779
arakno <paulobasto@gmail.com>
2021-11-15 10:48:13 +0000

    Move Panel component to file. Add type bindings.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,56 +1,24 @@
 import React, {Component} from 'react';
 import Button from './Button'
+import Panel from './Panel'
 
 
-const Panel = () => (
-  <div id="panel" className="panel-converter">
-    <table>
-      <tr>
-        <td><strong>wei</strong></td>
-        <td>1 wei</td>
-        <td>1</td>
-      </tr>
-      <tr>
-        <td><strong>Kwei (babbage)</strong></td>
-        <td>1e3 wei</td>
-        <td>1,000</td>
-      </tr>
-      <tr>
-        <td><strong>Mwei (lovelace)</strong></td>
-        <td>1e6 wei</td>
-        <td>1,000,000</td>
-      </tr>
-      <tr>
-        <td><strong>Gwei (shannon)</strong></td>
-        <td>1e9 wei</td>
-        <td>1,000,000,000</td>
-      </tr>
-      <tr>
-        <td><strong>microether (szabo)</strong></td>
-        <td>1e12 wei</td>
-        <td>1,000,000,000,000</td>
-      </tr>
-      <tr>
-        <td><strong>milliether (finney)</strong></td>
-        <td>1e15 wei</td>
-        <td>1,000,000,000,000,000</td>
-      </tr>
-      <tr>
-        <td><strong>ether</strong></td>
-        <td>1e18 wei</td>
-        <td>1,000,000,000,000,000,000</td>
-      </tr>
-    </table>
-  </div>
-)
 class Header extends Component<any, any> {
   constructor(props: any) {
     super(props)
     this.state = {
-        visible: false
+        visible: false,
+        value: 1
       }
     this.showPanel = this.showPanel.bind(this)
     this.hidePanel = this.hidePanel.bind(this)
+  }
+  
+  updateMessage = (gwei: number) => {
+    console.log(gwei);
+    this.setState(() => ({
+        value: gwei
+    }));
   }
 
   showPanel() {
@@ -62,11 +30,11 @@ class Header extends Component<any, any> {
   }
 
   render() {
-    const {visible} = this.state
+    const {visible, value} = this.state
 
     return (
       <header className="App-header">
-        {visible ? <Panel />: null}
+        {visible ? <Panel value={value} /> : null}
         <Button onClick={this.showPanel}
           text="show cenas"
         />
